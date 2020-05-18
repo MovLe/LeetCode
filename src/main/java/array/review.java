4709caf598638c986c45284ed1e6f8a0c550210f
Movle <richard.xiu.520@gmail.com>
2020-05-18 08:59:54 +0800

    LeetCode 复习之旋转矩阵

--- src/main/java/array/review.java
+++ src/main/java/array/review.java
@@ -122,7 +122,27 @@ public class review {
         }else {
             return result;
         }
+    }
 
+    public static void rotate(int[][] matrix){
+        int n =matrix.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int tem = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=tem;
+            }
+        }
+
+        int mid = n/2;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<mid;j++){
+                int tmp = matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=tmp;
+            }
+        }
 
     }
 }
