48ef5f83f7b8728174ad99eb2b9ef160c299e6e3
Movle <richard.xiu.520@gmail.com>
2020-05-19 08:27:34 +0800

    LeetCode 复习之返回倒数第k个结点的值

--- src/main/java/list/review.java
+++ src/main/java/list/review.java
@@ -42,4 +42,20 @@ public class review {
      return head;
 
     }
+
+    public static int kthToLast(ListNode head,int k){
+        ListNode p = head;
+
+        for(int i=0;i<k;i++){
+            p=p.next;
+        }
+
+        while(p!=null){
+            p=p.next;
+            head=head.next;
+        }
+
+        return head.val;
+
+    }
 }
